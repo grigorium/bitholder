8d49cd10e34714455a8bcde040dde7c53e8c5b22
Gregory <GAMashtaler@sberbank.ru>
2021-04-30 07:39:36 +0300

    Initial Commit

---
+++ BitHolder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitHolder
+//
+//  Created by grigori on 30.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
